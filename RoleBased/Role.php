9f6d6a2662b1901dd059134b3f42d2f2cff1ee20
Mathieu Savelli <mathieu.savelli@awakenweb.fr>
2014-01-12 14:30:39 +0100

    updated license text

--- RoleBased/Role.php
+++ RoleBased/Role.php
@@ -3,7 +3,7 @@
 /*
  * The MIT License
  *
- * Copyright 2013 Mathieu.
+ * Copyright 2013-2014 Mathieu SAVELLI <mathieu.savelli@awakenweb.fr>.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +23,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 namespace Iridium\Components\Permissions\RoleBased;
 
 class Role implements \IteratorAggregate
